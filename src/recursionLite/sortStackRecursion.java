a5a0dfae3741df95ac6661f8ebb763b37df73528
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2025-04-09 22:15:01 +0530

    sort an array using recursion

---
+++ src/recursionLite/sortStackRecursion.java
@@ -0,0 +1,39 @@
+package recursionLite;
+
+import java.util.Stack;
+
+public class sortStackRecursion {
+
+
+    import java.util.* ;
+import java.io.*;
+    public class Solution {
+
+        public static void sortStack(Stack<Integer> stack) {
+            // Write your code here.
+            sortRecur(stack);
+        }
+        private static void sortRecur(Stack<Integer> stack){
+            if(stack.size() == 1){
+                return;
+            }
+
+            int element = stack.pop();
+            sortRecur(stack);
+            insert(stack, element);
+        }
+
+        private static void insert(Stack<Integer> stack, int element){
+            if(stack.size() == 0 || stack.peek() <= element){
+                stack.push(element);
+                return;
+            }
+            int val = stack.pop();
+            insert(stack, element);
+            stack.push(val);
+            return;
+        }
+
+    }
+
+}
