2dca3dd790f835443521d3dfb5410121034199c7
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2025-04-28 23:06:08 +0530

    add print Subsequences

---
+++ src/recursionLite/printSubsequences.java
@@ -0,0 +1,30 @@
+package recursionLite;
+
+import java.util.ArrayList;
+
+public class printSubsequences {
+    public static void main(String[] args) {
+        String str = "abc";
+        StringBuilder input = new StringBuilder(str);
+        StringBuilder output = new StringBuilder("");
+        ArrayList<String> ans = new ArrayList<>();
+        solve(input, output, ans);
+        System.out.println(ans);
+    }
+    public static void solve(StringBuilder input, StringBuilder output, ArrayList<String> ans){
+        if(input.isEmpty()){
+            ans.add(output.toString());
+            return;
+        }
+        // create two outputs: one including the char, one excluding it
+        StringBuilder output1 = new StringBuilder(output); // exclude
+        StringBuilder output2 = new StringBuilder(output);
+        output2.append(input.charAt(0)); // include
+
+        // create the new input (after removing first character)
+        StringBuilder newInput = new StringBuilder(input.substring(1));
+
+        solve(newInput, output1, ans); // not including the character
+        solve(newInput, output2, ans); // including the character
+    }
+}
