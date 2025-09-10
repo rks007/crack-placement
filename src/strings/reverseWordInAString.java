607659f230c4f4132e461ed57405c5f55b935932
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2025-09-10 09:46:27 +0530

    add reverse Word In A String

---
+++ src/strings/reverseWordInAString.java
@@ -0,0 +1,31 @@
+package strings;
+
+public class reverseWordInAString {
+    public static void main(String[] args) { //leetcode 151
+
+        StringBuilder str = new StringBuilder(s);
+
+        str.reverse();
+        StringBuilder ans = new StringBuilder();
+
+        for(int i = 0; i < s.length(); i++){
+
+            StringBuilder helper = new StringBuilder();
+
+            while(i < s.length() && str.charAt(i) != ' '){
+                helper.append(str.charAt(i));
+                i++;
+            }
+
+            //then reverse the word that you got, so reverese the helper strig
+            helper.reverse();
+
+            if(helper.length() > 0){
+                ans.append(' ');
+                ans.append(helper);
+            }
+        }
+
+        return ans.substring(1).toString();
+    }
+}
